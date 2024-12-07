7ecf5bc4bb744a966c5ccdeb4c6e9a16e2f632f7
soila <elizabeth.soila@student.moringaschool.com>
2024-12-07 20:08:46 +0300

    phase-1-arrow-functions

---
+++ index.js
@@ -0,0 +1,16 @@
+let num1= 2000
+let num2=100
+
+function divide(value){
+const divide = num1/num2;
+return num1/num2;
+
+}
+
+const square = (parameter1) => parameter1**2;
+square(3); 
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(100,5); 
+
+
